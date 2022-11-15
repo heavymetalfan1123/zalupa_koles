b62937b347eb3ccd025c8ba40f4adbc8c2797076
heavymetalfan1123 <72998757+heavymetalfan1123@users.noreply.github.com>
2022-11-15 22:31:04 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,162 @@
+<?php
+echo "<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="initial-scale=1.0">
+  <title>index</title>
+  <link href="http://fonts.googleapis.com/css?family=Josefin+Slab:700" rel="stylesheet" type="text/css">
+  <style>body {
+  height: 100%;
+  background: url('../images/2gy8-full(2).jpg') rgb(255, 255, 255);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: left top;
+  background-attachment: fixed;
+  font: 700 4.25em/1.38 'Josefin Slab';
+  color: rgb(255, 255, 255);
+}
+
+.container {
+  float: left;
+}
+
+.element {
+  position: relative;
+  float: left;
+}
+
+.text {
+  float: left;
+  color: rgb(255, 255, 255);
+}
+
+.container-1 {
+  position: relative;
+  z-index: 2;
+  width: 100%;
+  height: 292px;
+  margin-top: -55px;
+}
+
+.container-2 {
+  position: relative;
+  clear: both;
+  z-index: 7;
+  width: 99.4166666666%;
+  height: 507px;
+  margin: -83px 0 0 0.5833333333%;
+}
+
+.container-3 {
+  clear: both;
+  width: 100%;
+  height: 459px;
+  margin-top: 164px;
+}
+
+.element-1 {
+  z-index: 0;
+  width: 100%;
+  height: 557px;
+  background-color: rgba(116, 117, 128, 0);
+}
+
+.element-2 {
+  clear: both;
+  z-index: 4;
+  width: 25.7502095557%;
+  height: 381px;
+  margin-left: 9.47061%;
+  border-bottom: 21px solid rgb(0, 0, 0);
+  border-radius: 32px;
+  background: url('../images/Vladimir_Lenin.jpg') rgb(222, 222, 222);
+  background-size: cover;
+  background-position: center center;
+}
+
+.element-3 {
+  z-index: 5;
+  width: 25.749161777%;
+  height: 381px;
+  margin-left: 1.61175%;
+  border-bottom: 21px solid rgb(0, 0, 0);
+  border-radius: 32px;
+  background: url('../images/d1c28756c16646898f81153f35c9d686.jpg') rgb(222, 222, 222);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+}
+
+.element-4 {
+  z-index: 6;
+  width: 25.749161777%;
+  height: 381px;
+  margin-left: 1.65339%;
+  border-bottom: 21px solid rgb(0, 0, 0);
+  border-radius: 32px;
+  background: url('../images/fec97b2821916413ff33ac2a09bf70df.png') rgb(222, 222, 222);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+}
+
+.text-1 {
+  position: relative;
+  clear: both;
+  z-index: 1;
+  width: 41.67%;
+  min-height: 66px;
+  margin: -458px 0 0 30.4%;
+  text-shadow: -2px 0 10000px rgba(0, 0, 0, .5), 2px 0 5px rgba(0, 0, 0, .5), 0 -2px 5px rgba(0, 0, 0, .5), 0 2px 5px rgba(0, 0, 0, .5);
+  font-size: 1.162em;
+  text-align: center;
+}
+
+.text-2 {
+  width: 61.5590947191%;
+  min-height: 105px;
+  margin: 95px 0 0 21.5423302598%;
+  text-shadow: -2px 0 5px rgba(0, 0, 0, .5), -1px 1px 5px rgba(0, 0, 0, .5), 0 2px 5px rgba(0, 0, 0, .5), 2px 1px 1px rgba(0, 0, 0, .5);
+  font: 400 1em/2.38 Helvetica;
+  text-align: center;
+}
+
+.text-3 {
+  width: 47.6%;
+  min-height: 127px;
+  margin: 65px 0 0 28.26563%;
+  font-family: Helvetica;
+  font-size: 1em;
+  font-weight: 400;
+  text-align: center;
+}
+
+.text-4 {
+  clear: both;
+  width: 80%;
+  min-height: 276px;
+  margin: 5px 0 0 10%;
+  font-size: 0.838em;
+}</style>
+</head>
+<body class="body page-index clearfix">
+  <div class="container container-1 clearfix">
+    <div class="element element-1"></div>
+    <p class="text text-1">T-Dream</p>
+  </div>
+  <div class="container container-2 clearfix">
+    <p class="text text-2">наша команда</p>
+    <div class="element element-2"></div>
+    <div class="element element-3"></div>
+    <div class="element element-4"></div>
+  </div>
+  <div class="container container-3">
+    <p class="text text-3">наши планы</p>
+    <p class="text text-4">loerem i[sum doloe sit amer rot ebal&nbsp;</p>
+  </div>
+</body>
+</html>"
+
+
+?>
